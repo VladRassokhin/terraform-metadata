7364adf44c8fe15841739e788771c7c82b8ae4f8
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-03-23 16:20:30 +0300

    Simplify fetch-providers.sh

--- schemas-extractor/fetch-providers.sh
+++ schemas-extractor/fetch-providers.sh
@@ -3,6 +3,5 @@
 pushd "$(dirname $0)" > /dev/null
 
 curl -s https://api.github.com/orgs/terraform-providers/repos?per_page=200 | jq '.[].name' -r | sort > providers.list.full
-cat providers.list.full | grep -- '-provider-' | awk -F '-' '{$1=$2=""; print substr($0, 3)}' | sed 's/ /-/' > providers.list
-
+cat providers.list.full | grep -- '^terraform-provider-' | awk '{print substr($0, 20)}' > providers.list
 popd > /dev/null
