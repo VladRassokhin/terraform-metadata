30d06f6d2722d02520c680b1a9b61138c77e8ec9
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2019-06-26 15:17:43 +0200

    add fetch-providers pagination

--- schemas-extractor/fetch-providers.sh
+++ schemas-extractor/fetch-providers.sh
@@ -2,6 +2,13 @@
 
 pushd "$(dirname $0)" > /dev/null
 
-curl -s https://api.github.com/orgs/terraform-providers/repos?per_page=200 | jq '.[].name' -r | sort > providers.list.full
+get_pages () {
+  local idx=1
+  while curl -s "https://api.github.com/orgs/terraform-providers/repos?sort=full_name&per_page=100&page=${idx}" | jq -re '.[].name' ; do
+    idx=$((idx + 1))
+  done
+}
+
+get_pages > providers.list.full
 cat providers.list.full | grep -- '^terraform-provider-' | awk '{print substr($0, 20)}' > providers.list
 popd > /dev/null
